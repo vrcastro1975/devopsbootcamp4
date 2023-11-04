fdc0e9a403ceda84e5ff8d1ec8897d5e49543035
vrcastro1975 <vrcastro1975@gmail.com>
2023-11-04 19:31:43 +0100

    01 Docker. Deberes 3 hechos

--- 01 Docker/contenedores-iii/deberes-iii.sh
+++ 01 Docker/contenedores-iii/deberes-iii.sh
@@ -12,6 +12,23 @@ npm start
 # Esto debería de generar el archivo Dockerfile dentro de hello-lemoncoder
 #O bien seleccionando el archivo Dockerfile con el botón derecho y hacer clic en Build Image...
 
+
+# He tenido que isntalar antes la extensión "Docker" de Microsoft en Visual Studio Code.
+# Una vez hecho esto, he seguido las instrucciones y he podido crear la imagen sin problema
+# Haciendo un "docker image ls" obtengo:
+# REPOSITORY        TAG       IMAGE ID       CREATED              SIZE
+# hellolemoncoder   latest    2c703766a3ea   About a minute ago   186MB
+
+
 # 2. Ejecutar un contenedor con tu nueva imagen usando Visual Studio Code
+
+# Para ejecutar un contenedor con la nueva imagen "hellolemoncoder", procedo de la siguiente manera:
+# docker run --name hellolemoncoder -p4000:3000 hellolemoncoder
+# Y obtengo lo siguiente:
+
+# > hello-lemoncoder@1.0.0 start
+# > node server.js
+
+# Example app listening on port 4000!
 
 
